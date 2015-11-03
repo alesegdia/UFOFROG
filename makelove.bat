52cdde6788333ea9fe68cc84cd3ae0043e533262
Alejandro Seguí <alesegdia@gmail.com>
2015-11-03 23:23:21 +0100

    removing old project structure

--- makelove.bat
+++
@@ -1,6 +0,0 @@
-cd game
-"C:\Program Files (x86)\7-Zip\7z.exe" a ufofrog.zip *
-mv ufofrog.zip ufofrog.love
-mv ufofrog.love ..
-cd ..
-"C:\Program Files\LOVE\love.exe" --console ufofrog.love
--- makewin.bat
+++
@@ -1,1 +0,0 @@
-copy /b love-0.9.1-win64\love.exe+%1 "%~n1.exe"
