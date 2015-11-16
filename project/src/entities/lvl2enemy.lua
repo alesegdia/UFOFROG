670b21ec972440c2129b3e425b85e93a4305c8a2
Alejandro Seguí <alesegdia@gmail.com>
2015-11-16 19:51:17 +0100

    modelling lvl2enemy behaviour

--- project/src/entities/lvl2enemy.lua
+++ project/src/entities/lvl2enemy.lua
@@ -6,6 +6,9 @@ local inspect = require 'libs.inspect'
 local helper_anim8 = require 'src.helpers.anim8'
 local helper_boxedit = require 'src.helpers.boxedit'
 
+local Timer = require "libs.hump.timer"
+local tween = Timer.tween
+
 require 'src.helpers.proxy'
 
 local Lvl2Enemy = Class {
@@ -20,12 +23,18 @@ function Lvl2Enemy:init(world)
 	local dtn = 1
 
 	local drtn = 0.5
-	self.anim = anim8.newAnimation( g(1,1, 2,1), {0.1, 0.3} )
+	self.anim = anim8.newAnimation( g(1,1, 2,1), {0.3, 0.1} )
 
-	self.body = {}
-	world:add(self.body, 0, 0, Image.lvl2enemy:getWidth() / 2, Image.lvl2enemy:getHeight() / 1)
+	self.body = { isEnemy = true }
+	world:add(self.body, 1000, love.math.random(0, 600), Image.lvl2enemy:getWidth() / 2, Image.lvl2enemy:getHeight() / 1)
 
 	self.isDead = false
+	self.speed = 0
+
+	local that = self
+	Timer.every(0.4, function()
+		that:advance()
+	end)
 
 end
 
@@ -35,9 +44,21 @@ function Lvl2Enemy:draw()
 	self.anim:draw(Image.lvl2enemy, x, y)
 end
 
+function Lvl2Enemy:advance()
+	self.speed = 400
+	tween(0.4, self, { speed = 200 }, 'out-expo' )
+end
+
+local col_filter = function(item, other)
+	if other.isBoss ~= true and not other.isEnemy ~= true then return nil end
+	if other.isPlayer == true then return "cross" end
+end
+
 function Lvl2Enemy:update(dt)
 
-	print(self.isDead)
+	local x, y, _, _ = self.world:getRect(self.body)
+	self.world:move(self.body, x - dt * self.speed, y, col_filter)
+
 	self.anim:update(dt)
 
 end
