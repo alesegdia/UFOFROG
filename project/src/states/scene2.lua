216f17f739f2ca68156d9213f4c0ea80ebc0e1b2
Alejandro Seguí <alesegdia@gmail.com>
2015-11-16 04:03:37 +0100

    adding enemies and handling

--- project/src/states/scene2.lua
+++ project/src/states/scene2.lua
@@ -3,27 +3,58 @@ local Timer = require "libs.hump.timer"
 local tween = Timer.tween
 local bump = require 'libs.bump'
 
+require 'libs.functional'
+
 local Lvl2Boss = require 'src.entities.lvl2boss'
 local Lvl2Hero = require 'src.entities.lvl2hero'
+local Lvl2Enemy = require 'src.entities.lvl2enemy'
 
 scene2 = {}
 
-local boss, hero
+local boss, hero, enemies
 local world
+enemies = {}
+
+local spawnEnemy = function(x, y)
+	local enemy = Lvl2Enemy( world )
+	table.insert(enemies, enemy)
+	return enemy
+end
+
+local clearEnemies = function()
+	enemies = filter(function (item) return item.isDead == false end, enemies)
+end
+
+local updateGroup = function(tbl, dt)
+	for _,item in ipairs(tbl) do
+		item:update(dt)
+	end
+end
+
+local drawGroup = function(tbl, dt)
+	for _,item in ipairs(tbl) do
+		item:draw(dt)
+	end
+end
 
 function scene2:enter()
 	world = bump.newWorld( 120 )
 	boss = Lvl2Boss( world )
 	hero = Lvl2Hero( world )
+	enemy = spawnEnemy(0,0)
 end
 
 function scene2:update(dt)
+
 	boss:update(dt)
 	hero:update(dt)
+	updateGroup(enemies, dt)
+	clearEnemies()
 end
 
 function scene2:draw()
 	boss:draw()
 	hero:draw()
+	drawGroup(enemies)
 end
 
