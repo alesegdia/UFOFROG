5b7d624bd6849150638d281251761503590cf0e1
Alejandro Seguí <alesegdia@gmail.com>
2015-11-17 22:39:21 +0100

    using shader effect as with scene1

--- project/src/states/scene2.lua
+++ project/src/states/scene2.lua
@@ -2,6 +2,7 @@
 local Timer = require "libs.hump.timer"
 local tween = Timer.tween
 local bump = require 'libs.bump'
+local cam 		= require (LIBRARYPATH.."hump.camera")
 
 require 'libs.functional'
 
@@ -14,6 +15,16 @@ scene2 = {}
 
 local boss, hero, entities
 local world
+local cam 		= cam.new(0,0,1,0)
+local shader_bg
+local shader_timer = 0
+local shake_current = 0
+
+local center = {
+	x = love.graphics.getWidth() / 2,
+	y = love.graphics.getHeight() / 2
+}
+
 entities = {}
 
 local spawnEnemy = function()
@@ -50,6 +61,7 @@ local drawGroup = function(tbl, dt)
 end
 
 function scene2:enter()
+	shader_bg = love.graphics.newShader( shadercombo )
 	world = bump.newWorld( 120 )
 	table.insert(entities, Lvl2Boss( world ))
 	table.insert(entities, Lvl2Hero( world ))
@@ -64,9 +76,42 @@ function scene2:update(dt)
 	clearGroup(entities)
 
 	Timer.update(dt)
+
+	-- update shader timer
+	shader_timer = shader_timer + dt * 10
+
+	-- hotline effect
+	local cam_scale = math.abs( 0.008 * math.sin( shader_timer / 10 ) ) + 0.9
+	cam:lookAt( center.x, center.y )
+	cam:zoomTo( cam_scale )
+	cam:rotateTo( 0.05 * math.sin( shader_timer / 10 ) )
+
+	-- divisor de intensidad para la epilepsia
+	local shader_shake_intensity = 20
+	local shader_shake_current = math.abs( shake_current / shader_shake_intensity )
+	if shader_shake_current > 1 then shader_shake_current = 1 end
+
+	-- rotacion para la psicodelia
+	local shader_pixel_rotation = -0.1*math.sin(5+shader_timer/10)
+
+	shader_bg:send( "time", 	shader_timer )
+	shader_bg:send( "factor",	shader_shake_current )
+	shader_bg:send( "angle", 	shader_pixel_rotation )
+
 end
 
 function scene2:draw()
+
+	-- render background
+	cam:attach()
+		love.graphics.setShader(shader_bg)
+		love.graphics.rectangle("fill", -100, -100, center.x*2 + 200, center.y*2 + 200 )
+		love.graphics.setShader()
+	cam:detach()
+
+	-- render stage
+	cam:attach()
 		drawGroup(entities)
+	cam:detach()
 end
 
