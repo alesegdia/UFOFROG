6c838717afa2413b1c3cfe53332f7efb81017d58
Alejandro Seguí <alesegdia@gmail.com>
2015-11-21 02:39:29 +0100

    working on boss behaviour and per-frame boxes

--- project/src/entities/lvl2boss.lua
+++ project/src/entities/lvl2boss.lua
@@ -3,6 +3,9 @@ local anim8 = require 'libs.anim8.anim8'
 local Class = require 'libs.hump.class'
 local inspect = require 'libs.inspect'
 
+local Timer = require "libs.hump.timer"
+local tween = Timer.tween
+
 local helper_anim8 = require 'src.helpers.anim8'
 local helper_boxedit = require 'src.helpers.boxedit'
 
@@ -15,63 +18,131 @@ local coords_to_index = function( c, r, cols )
 	return (r-1) * (cols) + c
 end
 
+function Lvl2Boss:forAllBodies(f)
+	for k,v in pairs(self.bodies) do
+		v:each(f)
+	end
+end
+
+function Lvl2Boss:loadBodies(num)
+	local bodies = {}
+	for i=1,num do
+		local bodygroup = helper_boxedit.bump.createFrameBodyData( self.world, self.provider, i )
+		table.insert(bodies, bodygroup)
+	end
+	return bodies
+end
+
+function Lvl2Boss:configBodiesForAnim()
+	for k,v in pairs(self.bodies) do
+		self.anim:assignFrameStart(k, function(anim)
+			for k2,v2 in pairs(self.bodies) do
+				if k2 == k then
+					v2:activate()
+					self.currentBodyData = v2
+				else
+					v2:deactivate()
+				end
+			end
+		end)
+	end
+end
+
 function Lvl2Boss:init(world)
 
 	self.world = world
 
+	self.x = 800
+
+	self.y = 0
+
 	-- physic definition loading
-	local provider = helper_boxedit.newProvider('data/boss2_def.json', 2, 4)
-	self.bodies = {}
-	self.bodies[1] = helper_boxedit.bump.createFrameBodyData( world, provider, coords_to_index(1, 1, 2) )
-	self.bodies[1]:each( function( element ) element.isBoss = true end )
+	self.provider = helper_boxedit.newProvider('data/boss2_def.json', 2, 4)
 
-	self.bodies[2] = helper_boxedit.bump.createFrameBodyData( world, provider, coords_to_index(1, 2, 2) )
-	self.bodies[2]:each( function( element ) element.isBoss = true end )
-
-	self.bodies[3] = helper_boxedit.bump.createFrameBodyData( world, provider, coords_to_index(2, 2, 2) )
-	self.bodies[3]:each( function( element ) element.isBoss = true end )
+	self.bodies = self:loadBodies(8)
+	self:forAllBodies(function(element) element.isBoss = true end)
 
 	-- animation loading
-	local g = helper_anim8.newGrid(Image.lvl2boss, 2, 4)
+	local g = helper_anim8.newGrid(Image.lvl2boss_nocolor, 2, 4)
 	local dtn = 1
-	self.anim = anim8.newAnimation( g(1,1, 1,2, 2,2), {2, 0.1, 5.0} )
+	--self.anim = anim8.newAnimation( g(1,1, 1,2, 2,2), {2, 0.1, 5.0} )
+	self.anim = anim8.newAnimation( g(1,1, 2,1), {0.4, 0.4} )
 
-	local that = self
-	self.anim:assignFrameStart(1, function(anim)
-		that.currentBodyData = that.bodies[1]
-		that.bodies[1]:activate()
-		that.bodies[2]:deactivate()
-		that.bodies[3]:deactivate()
-	end)
-
-	self.anim:assignFrameStart(2, function(anim)
-		that.currentBodyData = that.bodies[2]
-		that.bodies[1]:deactivate()
-		that.bodies[2]:activate()
-		that.bodies[3]:deactivate()
-	end)
-
-	self.anim:assignFrameStart(3, function(anim)
-		that.currentBodyData = that.bodies[3]
-		that.bodies[1]:deactivate()
-		that.bodies[2]:deactivate()
-		that.bodies[3]:activate()
-	end)
+	self:configBodiesForAnim()
 
 	self.currentBodyData = self.bodies[1]
+
+	local theboss = self
+
+	self.states = {
+		wandering = {
+			timerhandle = {},
+			enter = function(self)
+				self.timerhandle = tween(1, theboss, {y = -70}, 'in-out-circ', function()
+					self.timerhandle = tween(1, theboss, {y = 150}, 'in-out-circ', function()
+						theboss:changeState(theboss.states.wandering)
+					end)
+				end)
+			end,
+			leave = function(self)
+				Timer.cancel(self.timerhandle)
+			end
+		},
+		arriving = {
+			timerhandle = {},
+			enter = function(self)
+				self.x = 800
+				self.y = 0
+				self.timerhandle = tween(5, theboss, {x = 430}, "out-back", function()
+					theboss:changeState(theboss.states.wandering)
+				end)
+			end,
+			leave = function(self)
+				Timer.cancel(self.timerhandle)
+			end
+		},
+
+	}
+
+
+	self.currentState = self.states.arriving
+	self:changeState(self.states.arriving)
+
+end
+
+function Lvl2Boss:changeState(newState)
+	self.currentState:leave()
+	self.currentState = newState
+	self.currentState:enter()
+end
+
+function Lvl2Boss:explode()
 
 end
 
 function Lvl2Boss:draw()
 	for k,v in pairs(self.currentBodyData.data) do
 		local x, y, w, h = self.world:getRect(v)
-		--love.graphics.rectangle("line", x, y, w, h)
+		love.graphics.rectangle("line", x, y, w, h)
 	end
-	self.anim:draw(Image.lvl2boss, 0, 0)
+	--love.graphics.setColor(255, 255, 0, 255)
+	self.anim:draw(Image.lvl2boss_nocolor, self.x, self.y)
+	--love.graphics.setColor(255, 255, 255, 255)
+end
+
+local col_filter = function(item, other)
+	if item.isActive then return "cross" end
+	return nil
 end
 
 function Lvl2Boss:update(dt)
 	self.anim:update(dt)
+	self.currentBodyData:each( function( element )
+		local aX, aY = self.world:getRect(element)
+		local newx = element.x + self.x
+		local newy = element.y + self.y
+		self.world:move(element, newx, newy, col_filter)
+	end )
 end
 
 return Lvl2Boss
