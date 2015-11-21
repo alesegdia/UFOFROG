cc2cc9d69bb5ae492c4cc6eaaa40442a0e326760
Alejandro Seguí <alesegdia@gmail.com>
2015-11-21 02:40:21 +0100

    adding smoke, explosion when dead and some collision handling to enemy

--- project/src/entities/lvl2enemy.lua
+++ project/src/entities/lvl2enemy.lua
@@ -9,24 +9,27 @@ local helper_boxedit = require 'src.helpers.boxedit'
 local Timer = require "libs.hump.timer"
 local tween = Timer.tween
 
+local Lvl2Smoke = require 'src.entities.lvl2smoke'
+local Lvl2Explosion = require 'src.entities.lvl2explosion'
+
 require 'src.helpers.proxy'
 
 local Lvl2Enemy = Class {
 }
 
-function Lvl2Enemy:init(world)
+function Lvl2Enemy:init(world, stage)
 
 	self.world = world
+	self.stage = stage
 
 	-- animation loading
 	local g = helper_anim8.newGrid(Image.lvl2enemy, 2, 1)
-	local dtn = 1
 
 	local drtn = 0.5
 	self.anim = anim8.newAnimation( g(1,1, 2,1), {0.3, 0.1} )
 
-	self.body = { isEnemy = true }
-	world:add(self.body, 1000, love.math.random(0, 400), Image.lvl2enemy:getWidth() / 2, Image.lvl2enemy:getHeight() / 1)
+	self.body = { isEnemy = true, entity = self, isBaba = true }
+	world:add(self.body, 1200, love.math.random(0, 400), Image.lvl2enemy:getWidth() / 2, Image.lvl2enemy:getHeight() / 1)
 
 	self.isDead = false
 	self.speed = 0
@@ -44,22 +47,46 @@ function Lvl2Enemy:draw()
 	self.anim:draw(Image.lvl2enemy, x, y)
 end
 
+function Lvl2Enemy:explode()
+
+	local x = self.x
+	local y = self.y
+
+	Timer.after(0, function()
+		table.insert(self.stage, Lvl2Explosion(x + 40 + math.random(10,20), y+40 + math.random(1,10), 0.75))
+	end)
+	Timer.after(0.2, function()
+		table.insert(self.stage, Lvl2Explosion(x + 40, y + 40, 1))
+	end)
+	self.isDead = true
+end
+
+function Lvl2Enemy:die()
+	self.world:remove(self.body)
+end
+
 function Lvl2Enemy:advance()
-	self.speed = 400
-	tween(0.4, self, { speed = 200 }, 'out-expo' )
+	self.speed = 5000
+	tween(0.4, self, { speed = 1000 }, 'out-expo' )
 end
 
 local col_filter = function(item, other)
 	if other.isBoss ~= true and not other.isEnemy ~= true then return nil end
-	if other.isPlayer == true then return "cross" end
+	if other.isPlayer == true or other.isBullet then return "cross" end
 end
 
 function Lvl2Enemy:update(dt)
 
 	local x, y, _, _ = self.world:getRect(self.body)
-	self.world:move(self.body, x - dt * self.speed, y, col_filter)
+	local aX, aY, len, cols = self.world:move(self.body, x - dt * self.speed, y, col_filter)
+
+	self.x, self.y = aX, aY
 
 	if x < -400 then self.isDead = true end
+
+	if self.speed > 2000 then
+			table.insert(self.stage, Lvl2Smoke(x+80, y+40, 0.8, {r=255, g=0, b=0}))
+	end
 
 	self.anim:update(dt)
 
